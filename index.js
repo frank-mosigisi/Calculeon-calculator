446d1b4d0499d23dcf517d3c5e75fee4d5b1f0ec
frank-mosigisi <frankmosigisi427@gmail.com>
2024-05-02 22:27:28 -0500

    create basic calculator functions

---
+++ index.js
@@ -0,0 +1,21 @@
+function add (a,b){
+    return a + b
+}
+
+function subtract (a,b){
+    return a - b
+}
+
+function multiplication (a,b){
+    return a * b
+}
+
+function division (a,b){
+    return a / b
+}
+
+
+console.log(add(2,3))
+console.log(subtract(2,3))
+console.log(multiplication(2,3))
+console.log(division(4,2))
