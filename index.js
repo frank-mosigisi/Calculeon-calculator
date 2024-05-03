aa05830de7ce613d540a5f7cdb667180f4f137c7
frank-mosigisi <frankmosigisi427@gmail.com>
2024-05-03 02:50:41 -0500

    created html calculator section

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ function division (a,b){
 
 
 // console.log(add(2,3))
-// console.log(subtract(2,3))
-// console.log(multiplication(2,3))
-// console.log(division(4,2))
 
 let numberOne
 let numberTwo
@@ -45,3 +42,38 @@ function operate (numberOne, operater, numberTwo){
     }
 }
 
+//buttons creation
+
+function createButton( id, text){
+    let button = document.createElement("button")
+    button.innerHTML= text
+    // button.id = id;
+    // button.addEventListener("click", function(){
+    //     alert('button' + id + "clicked")
+    // });
+    button.classList.add('NumberButtons')
+    return button
+}
+
+function addButtons() {
+
+    let buttonOrder = ['AC','DEL','%',"÷",7,8,9,"x",4,5,6,"-",1,2,3,"+",0,".","="]
+    let container = document.getElementById("button-container");
+    for (let i = 0; i < buttonOrder.length; i++) {
+        let buttonId = "button" + i;
+        let buttonText = buttonOrder[i];
+        let newButton = createButton(buttonId, buttonText);
+        newButton.classList.add("button-" + buttonText)
+        container.appendChild(newButton);
+
+    
+    (buttonOrder[i] === '%') ?newButton.classList.add('modulusButton'):
+    (buttonOrder[i] === '÷') ?newButton.classList.add('modulusButton'):
+    (buttonOrder[i] === 'x') ?newButton.classList.add('modulusButton'):
+    (buttonOrder[i] === '-') ?newButton.classList.add('modulusButton'):
+    (buttonOrder[i] === '+') ?newButton.classList.add('modulusButton'):
+    (buttonOrder[i] === '=') ?newButton.classList.add('modulusButton'):
+    '';
+    }
+}
+addButtons()
